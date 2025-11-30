c7d07598e068ee47b67e9f6220e1884558b2299e
binita.subedi <binita.subedi@infodev.com.np>
2025-11-30 16:32:15 +0545

    load pages

---
+++ Rest-API/q5.js
@@ -0,0 +1,10 @@
+async function loadPages() {
+  const [p1, p2] = await Promise.all([
+    fetch('https://jsonplaceholder.typicode.com/posts?_page=1').then(r=>r.json()),
+    fetch('https://jsonplaceholder.typicode.com/posts?_page=2').then(r=>r.json())
+  ]);
+  return [...p1, ...p2];
+}
+loadPages().then(result => {
+    console.log(result);
+})
