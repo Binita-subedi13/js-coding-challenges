d6f73f405e9b427d4baa19395fc84072e7dfb885
binita.subedi <binita.subedi@infodev.com.np>
2025-11-30 15:36:21 +0545

    safe access with destructure

---
+++ destructuring-spread-rest/q5.js
@@ -0,0 +1,10 @@
+function getAvatar(user) {
+  return user?.profile?.avatar?.url ?? 'No Avatar';
+}
+console.log(getAvatar({
+  profile: { avatar: { url: "img.png" } }
+}))
+
+console.log(
+  getAvatar({ profile: {} })
+)
