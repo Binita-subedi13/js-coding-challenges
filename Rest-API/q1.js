64e912a18e2cd3c663c227b7f2efcf599edc41b7
binita.subedi <binita.subedi@infodev.com.np>
2025-11-30 16:26:56 +0545

    rest api

---
+++ Rest-API/q1.js
@@ -0,0 +1,8 @@
+async function getUsers() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+  return data.map(u => u.name);
+}
+getUsers().then(result => {
+  console.log(result);
+})
