82f01cb83e3eca19ecade53d4f3276e17ecd66e9
binita.subedi <binita.subedi@infodev.com.np>
2025-11-30 16:28:47 +0545

    rest api token

---
+++ Rest-API/q3.js
@@ -0,0 +1,20 @@
+async function createPost()
+{
+    try
+    {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts/1', {
+                                headers: {
+                                    'Content-type': 'application/json; charset=UTF-8',
+                                    'Authorization':'Bearer 12345'
+                                },
+                                });
+        const data = await response.json();
+        return data;
+    }
+    catch(error)
+    {
+        console.error("Error occur:", error);
+    }
+}
+
+createPost().then(result => console.log(result));
